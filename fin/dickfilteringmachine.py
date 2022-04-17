d43ef9ad43a13b170916ce429a6c20ee6485fa7c
Norah (Jo) Vii <70073104+norahvii@users.noreply.github.com>
2022-04-17 16:56:32 -0500

    Update dickfilteringmachine.py

--- fin/dickfilteringmachine.py
+++ fin/dickfilteringmachine.py
@@ -1,7 +1,11 @@
 import collections
 import re
 
-# Create a list for all the stop words and not new lines / commented lines
+# Create a list for all the iterables in the book
+book = re.findall(r'\w+',open('mobydick.txt').read().lower())
+
+
+# Create a list for all stop words
 stop_words = []
 valid = re.compile(r'^\b[a-z]+\b')
 
@@ -10,9 +14,7 @@ with open('stop-words.txt', 'r') as text_file:
         if valid.match(line) != None:
             stop_words.append(line)
 
-# Create a list for all the iterables in the book
-book = re.findall(r'\w+',open('mobydick.txt').read().lower())
-
+            
 # Create a list for the book filtered by the stop word list
 filtered_book = []
 
@@ -22,5 +24,6 @@ for elem in book:
 
 # List the most common words in the filtered book
 most_common = collections.Counter(filtered_book).most_common(100)
+
 # Print it
 print(most_common)
