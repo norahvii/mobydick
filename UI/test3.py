59528d4acd5a9b041ce5d8384e80f9be5e378486
jopvivi <joe66ano@gmail.com>
2022-04-18 09:47:28 -0500

    exe

--- UI/test3.py
+++ UI/test3.py
@@ -6,10 +6,15 @@ layout = [[sg.Text('Add book')],
           [sg.Input(), sg.FileBrowse()],
           [sg.Text('Add stop list')],
           [sg.Input(), sg.FileBrowse()],
-          [sg.OK(), sg.Cancel()]
+          [sg.Button('Run'), sg.Button('Quit')]
           ]
 
 window = sg.Window('Add files', layout)
 
-event, values = window.read()
+while True:
+    event, values = window.read()
+    if event == 'Quit':
+        windowclose()
+
+
 windowclose()
